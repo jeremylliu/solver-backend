9b0b6d23feb0afc9bb85f56e09aac825407815f0
Jeremy <jliuu@umich.edu>
2021-08-07 14:36:24 -0500

    fixed bug with invalid image

--- computer_vision/convert.py
+++ computer_vision/convert.py
@@ -4,7 +4,7 @@ import io
 
 def base64ToImage(imageStr):
     # takes in a base64 imageStr and returns a jpeg object
-    if len(imageStr) > 22:
+    if not imageStr or len(imageStr) > 22:
         imageStr = imageStr[22:]
     img = io.BytesIO(base64.b64decode(imageStr))
     return img.read()
