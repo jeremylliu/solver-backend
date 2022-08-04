6981570fdb9eb8fda35880d93ad33a9acafd7673
Jeremy Liu <jliuu@umich.edu>
2022-08-03 22:42:37 -0500

    fixed and dockerized api to run on localhost

--- app.py
+++ app.py
@@ -1,18 +1,14 @@
 import cv2
 from views import *
 
-app = flask.Flask(__name__)
 
-
-def create_app(config_file="settings.py"):
+def create_app():
     app = flask.Flask(__name__)
-
-    app.config.from_pyfile(config_file)
-
     app.register_blueprint(main)
 
     return app
 
 
 if __name__ == '__main__':
-    app.run(threaded=True, port=3001)
+    app = create_app()
+    app.run(threaded=True, host="0.0.0.0", port=3001)
